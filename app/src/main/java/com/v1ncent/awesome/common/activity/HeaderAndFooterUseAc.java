487ec6c19f11fafbe3033b08f7b15164f6198064
陈静 <1041066648@qq.com>
2017-06-19 10:12:08 +0800

    test

---
+++ app/src/main/java/com/v1ncent/awesome/common/activity/HeaderAndFooterUseAc.java
@@ -0,0 +1,16 @@
+package com.v1ncent.awesome.common.activity;
+
+import android.view.View;
+
+import com.v1ncent.awesome.base.BaseActivity;
+
+/**
+ * Created by v1ncent on 2017/6/19.
+ */
+
+public class HeaderAndFooterUseAc extends BaseActivity {
+    @Override
+    public void onClickListener(View v) {
+
+    }
+}
